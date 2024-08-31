0f1d017595721615c87696de8f14d46e4ef4c84e
gokayfem <civileng.gokay@gmail.com>
2024-09-01 01:45:21 +0300

    warning

--- ui_components.py
+++ ui_components.py
@@ -97,7 +97,8 @@ def create_interface():
                     clip_g_output = gr.Textbox(label="CLIP G Output", visible=True)
             
             with gr.Column(scale=2):
-                with gr.Accordion("Prompt Generation with LLM (You need to use Generate Prompt first)", open=False):
+                with gr.Accordion("""Prompt Generation with LLM 
+                                  (You need to use Generate Prompt first)""", open=False):
                     happy_talk = gr.Checkbox(label="Happy Talk", value=True)
                     compress = gr.Checkbox(label="Compress", value=True)
                     compression_level = gr.Radio(["soft", "medium", "hard"], label="Compression Level", value="hard")
